5c7c5a74563253b14a52259ac23c6695440dc67d
sappy920 <sae.920@gamil.com>
2021-02-23 16:02:56 +0900

    マイページの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users
   root to: 'toppages#index'
   resources :articles, only: [:index, :new, :create, :show, :destroy, :edit]
+  resources :users, only: :show
 end
