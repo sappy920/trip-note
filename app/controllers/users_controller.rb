1377d369c50915dc805556365f9aa53840a452c8
sappy920 <sae.920@gamil.com>
2021-02-25 23:46:52 +0900

    userコントローラーの設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
-    @nickname = current_user.nickname
-    @articles = current_user.articles
+    @user = User.find(params[:id])
   end
-end
+end  
+
