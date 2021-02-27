db7c878d880b549b8801d4b0e19efc50e572fd7d
sappy920 <sae.920@gamil.com>
2021-02-27 22:01:09 +0900

    usersコントローラの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,10 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  private
+  def user_params
+    params.require(:user).permit(:nickname, :email, :birthday, :profile, :profile_image)
+  end
 end  
 
