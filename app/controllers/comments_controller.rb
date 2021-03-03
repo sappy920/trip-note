bd0690e83aedb837d9760b4d1c8fb9c72435d9e3
sappy920 <sae.920@gamil.com>
2021-03-03 23:34:12 +0900

    コメントコントローラの修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,12 @@
 class CommentsController < ApplicationController
   def create
-    Comment.create(comment_params)
-    redirect_to "/articles/#{comment.article.id}"
+    @comment = Comment.new(comment_params)
+    @comment.user_id = current_user.id
+    if @comment.save
+        redirect_to articles_path
+    else
+        redirect_to root_path
+    end
   end
 
   private
