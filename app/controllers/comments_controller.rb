3b3783d1d9713725ee21e5a03def9d3d21510461
sappy920 <sae.920@gamil.com>
2021-03-05 22:58:40 +0900

    コメント削除機能の追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,7 @@
 class CommentsController < ApplicationController
+  before_action :set_article
+  before_action :authenticate_user!
+
   def create
     @comment = Comment.new(comment_params)
     @comment.user_id = current_user.id
@@ -9,7 +12,24 @@ class CommentsController < ApplicationController
     end
   end
 
+
+  def destroy
+    @comment = @article.comments.find(params[:id])
+    if @comment.destroy
+      redirect_to article_path(@article)
+    else
+      render article_path(@article)
+    end
+  end
+
+  
+
   private
+
+  def set_article
+    @article = Article.find(params[:article_id])
+  end
+
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, article_id: params[:article_id])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   get 'users/show'
   devise_for :users
   root to: 'toppages#index'
+
   resources :articles do
-    resources :comments, only: :create
+    resources :comments, only: [:create, :destroy]
   end
+
   resources :users, only: [:show, :edit, :update]
 end
