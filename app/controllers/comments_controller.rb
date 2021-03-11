7946e491ec8cc771c0390b9d402ba439266eebd6
sappy920 <sae.920@gamil.com>
2021-03-11 12:56:54 +0900

    コメント投稿機能の編集

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,7 +6,7 @@ class CommentsController < ApplicationController
     @comment = Comment.new(comment_params)
     @comment.user_id = current_user.id
     if @comment.save
-        redirect_to articles_path
+        redirect_to article_path(@article)
     else
         redirect_to root_path
     end
